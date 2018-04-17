bedfbd365cee479987797af515110ca1c34ec89e
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-04-17 19:55:06 +0200

    Add BtcInfo

---
+++ src/components/BtcInfo.ts
@@ -0,0 +1,21 @@
+// Copyright (C) 2018 Andreas Huber Dönni
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+
+import { CryptoAssetInfo } from "./CryptoAssetInfo";
+
+/** Provides information about a BTC asset, */
+export class BtcInfo extends CryptoAssetInfo {
+    /** Creates a new [[BtcInfo]] instance. */
+    public constructor(location: string, description: string, quantity: number) {
+        super(location, description, "BTC", quantity, 8, "bitcoin");
+    }
+}
