8cf78ed7df8018dd1a5eb5395b06ee1e0c84da6f
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-05-02 13:22:35 +0200

    Eliminate QuandlParser

--- src/components/QuandlRequest.ts
+++ src/components/QuandlRequest.ts
@@ -11,7 +11,6 @@
 // <http://www.gnu.org/licenses/>.
 
 import { IWebRequest } from "./IWebRequest";
-import { QuandlParser } from "./QuandlParser";
 import { QueryCache } from "./QueryCache";
 
 export class QuandlRequest implements IWebRequest<number> {
@@ -22,11 +21,39 @@ export class QuandlRequest implements IWebRequest<number> {
         if (this.path.length > 0) {
             const response = await QueryCache.fetch(
                 `https://www.quandl.com/api/v3/datasets/${this.path}?api_key=ALxMkuJx2XTUqsnsn6qK&rows=1`);
-            const price = QuandlParser.getPrice(response);
+            const price = QuandlRequest.getPrice(response);
 
             return this.invert ? 1 / price : price;
         } else {
             return 1;
         }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static getPrice(response: any) {
+        return this.hasDatasetProperty(response) && this.hasDataTupleArray(response.dataset) ?
+            response.dataset.data[0][1] : Number.NaN;
+    }
+
+    private static hasDatasetProperty(value: any): value is { dataset: object } {
+        return this.hasStringIndexer(value) && (value.dataset instanceof Object);
+    }
+
+    private static hasDataTupleArray(value: any): value is { data: Array<[ string, number ]> } {
+        return this.hasDataArrayArray(value) && (value.data[0].length >= 2) &&
+            (typeof value.data[0][0] === "string") && (typeof value.data[0][1] === "number");
+    }
+
+    private static hasDataArrayArray(value: any): value is { data: any[][] } {
+        return this.hasDataArray(value) && (value.data.length >= 1) && (value.data[0] instanceof Array);
+    }
+
+    private static hasDataArray(value: any): value is { data: any[] } {
+        return this.hasStringIndexer(value) && (value.data instanceof Array);
+    }
+
+    private static hasStringIndexer(value: any): value is { [key: string]: any } {
+        return value instanceof Object;
     }
 }
