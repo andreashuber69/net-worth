c0bcc482b3d891f4f598e76f4e852055ad48ed8d
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-05-27 13:20:11 +0200

    Have the npm build script build for deployment on gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    // baseUrl needs to be set differently depending on whether we build for actual deployment (e.g. on github gh-pages)
+    // or for local production mode testing. The folling uses an exported environment variable to do just that.
+    // Obviously, this needs to bet set accordingly before building for deployment. 
+    baseUrl: process.env.WEBPACK_BASE_URL ? process.env.WEBPACK_BASE_URL : "/",
     chainWebpack: config => {
         // The following tweaks are necessary because bitcoinjs-lib requires that certain class names are not mangled,
         // see https://github.com/bitcoinjs/bitcoinjs-lib/issues/659.
