a33d07d0c82174b901ecc77a4d446ebdcae17138
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-06-13 17:04:51 +0200

    Make EthBundle a nested class of EthWallet

--- src/model/EthWallet.ts
+++ src/model/EthWallet.ts
@@ -10,11 +10,19 @@
 // You should have received a copy of the GNU General Public License along with this program. If not, see
 // <http://www.gnu.org/licenses/>.
 
-import { IModel } from "./Asset";
+import { Asset, IModel } from "./Asset";
 import { AssetBundle } from "./AssetBundle";
 import { CryptoWallet, ICryptoWalletProperties } from "./CryptoWallet";
-import { EthBundle } from "./EthBundle";
+import { Erc20TokenWallet } from "./Erc20TokenWallet";
 import { EtherscanEthBalanceRequest } from "./EtherscanEthBalanceRequest";
+import { EtherscanTokenBalanceRequest } from "./EtherscanTokenBalanceRequest";
+import { QueryCache } from "./QueryCache";
+import { Unknown, Value } from "./Value";
+
+interface ITokenInfo {
+    readonly contractAddress: string;
+    readonly decimals: number;
+}
 
 /** Represents an ETH wallet. */
 export class EthWallet extends CryptoWallet {
@@ -35,10 +43,123 @@ export class EthWallet extends CryptoWallet {
     }
 
     public bundle(): AssetBundle {
-        return new EthBundle(this);
+        return new EthWallet.EthBundle(this);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // tslint:disable-next-line:max-classes-per-file variable-name
+    private static readonly EthBundle = class NestedEthBundle extends AssetBundle {
+        public readonly assets: Asset[] = [];
+
+        /** @internal */
+        public constructor(wallet: EthWallet) {
+            super();
+            this.assets.push(wallet);
+            this.addTokenWallets().catch((reason) => console.error(reason));
+        }
+
+        public deleteAsset(asset: Asset) {
+            const index = this.assets.indexOf(asset);
+
+            if (index >= 0) {
+                this.assets.splice(index, 1);
+            }
+        }
+
+        /** @internal */
+        public toJSON() {
+            return [ this.assets[0].toJSON() ];
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private static delay(milliseconds: number) {
+            return new Promise<void>((resolve) => setTimeout(resolve, milliseconds));
+        }
+
+        private static async getKnownTokens() {
+            const knownTokens = await QueryCache.fetch(
+                "https://raw.githubusercontent.com/kvhnuke/etherwallet/mercury/app/scripts/tokens/ethTokens.json");
+
+            if (Value.isArray(knownTokens)) {
+                const result = new Map<string, ITokenInfo>();
+
+                for (const token of knownTokens) {
+                    if (Value.hasStringProperty(token, "address") && Value.hasStringProperty(token, "symbol") &&
+                        Value.hasNumberProperty(token, "decimal")) {
+                        result.set(token.symbol, { contractAddress: token.address, decimals: token.decimal });
+                    }
+                }
+
+                return result;
+            }
+
+            return undefined;
+        }
+
+        private get ethWallet() {
+            return this.assets[0] as EthWallet;
+        }
+
+        private async addTokenWallets() {
+            if (!this.ethWallet.address) {
+                return;
+            }
+
+            const knownTokens = await NestedEthBundle.getKnownTokens();
+
+            if (!knownTokens) {
+                return;
+            }
+
+            const currencies = await QueryCache.fetch("https://api.coinmarketcap.com/v2/ticker/");
+
+            if (!Value.hasObjectProperty(currencies, "data")) {
+                return;
+            }
+
+            const data = currencies.data;
+
+            for (const id in data) {
+                if (data.hasOwnProperty(id)) {
+                    await this.addTokenWallet(knownTokens, this.ethWallet.address, data[id]);
+                }
+            }
+        }
+
+        private async addTokenWallet(
+            knownTokens: Map<string, ITokenInfo>, address: string, ticker: Unknown | null | undefined) {
+            if (!Value.hasStringProperty(ticker, "symbol") || !Value.hasStringProperty(ticker, "website_slug") ||
+                !Value.hasObjectProperty(ticker, "quotes") || !Value.hasObjectProperty(ticker.quotes, "USD")) {
+                return;
+            }
+
+            const knownToken = knownTokens.get(ticker.symbol);
+
+            if (!knownToken) {
+                return;
+            }
+
+            const usdQuotes = ticker.quotes.USD;
+
+            if (!Value.hasNumberProperty(usdQuotes, "price")) {
+                return;
+            }
+
+            const balance = await new EtherscanTokenBalanceRequest(
+                address, knownToken.contractAddress, knownToken.decimals).execute();
+
+            if (balance > 0) {
+                const newProperties = { ...this.ethWallet as ICryptoWalletProperties, quantity: balance };
+                this.assets.push(
+                    new Erc20TokenWallet(this.ethWallet.parent, newProperties, ticker.symbol, ticker.website_slug));
+            }
+
+            // Etherscan will answer at most 5 requests per second. This should push it well below that limit.
+            await NestedEthBundle.delay(300);
+        }
+    };
 
     private async queryQuantity() {
         if (this.address) {
