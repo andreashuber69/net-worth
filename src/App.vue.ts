a42cd739bd5670cd6bdac7c49e84261f60bf6d35
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-07-16 15:57:53 +0200

    Ensure safe use in multiple browser windows
    Closes #36

--- src/App.vue.ts
+++ src/App.vue.ts
@@ -24,7 +24,8 @@ export default class App extends Vue {
 
     public constructor() {
         super();
-        this.model = this.initModel(App.parse(undefined, App.loadFromLocalStorage()) || new Model());
+        this.model = this.initModel(App.loadFromLocalStorage() || new Model());
+        window.addEventListener("beforeunload", (ev) => this.onBeforeUnload(ev));
     }
 
     public onMenuClicked(event: MouseEvent) {
@@ -80,24 +81,23 @@ export default class App extends Vue {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private static readonly assetsKey = "assets";
+    private static readonly sessionStorageKey = "localStorageKey";
 
-    private static write(filename: string, blob: Blob) {
-        if (window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveBlob(blob, filename);
-        } else {
-            const elem = window.document.createElement("a");
+    private static loadFromLocalStorage() {
+        const localStorageKey = App.getLocalStorageKey();
 
-            // We should call window.URL.revokeObjectURL as soon as the user has either successfully downloaded the
-            // file or cancelled the download, but there seems to be no reliable way to detect these events. According
-            // to the docs the objects will be garbage collected anyway when the user closes the tab or navigates away.
-            // Given the currently small size of these downloads, not calling revokeObjectURL is probably good enough.
-            elem.href = window.URL.createObjectURL(blob);
-            elem.download = filename;
-            document.body.appendChild(elem);
-            elem.click();
-            document.body.removeChild(elem);
+        if (localStorageKey) {
+            const model = App.parse(undefined, window.localStorage.getItem(localStorageKey));
+
+            if (model) {
+                window.sessionStorage.removeItem(this.sessionStorageKey);
+                window.localStorage.removeItem(localStorageKey);
+
+                return model;
+            }
         }
+
+        return undefined;
     }
 
     private static read(blob: Blob) {
@@ -122,8 +122,6 @@ export default class App extends Vue {
                 }
             }
 
-            this.saveToLocalStorage(model.toJsonString());
-
             return model;
         } else if (model) {
             alert(model);
@@ -132,12 +130,123 @@ export default class App extends Vue {
         return undefined;
     }
 
-    private static saveToLocalStorage(json: string) {
-        window.localStorage.setItem(this.assetsKey, json);
+    private static write(filename: string, blob: Blob) {
+        if (window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveBlob(blob, filename);
+        } else {
+            const elem = window.document.createElement("a");
+
+            // We should call window.URL.revokeObjectURL as soon as the user has either successfully downloaded the
+            // file or cancelled the download, but there seems to be no reliable way to detect these events. According
+            // to the docs the objects will be garbage collected anyway when the user closes the tab or navigates away.
+            // Given the currently small size of these downloads, not calling revokeObjectURL is probably good enough.
+            elem.href = window.URL.createObjectURL(blob);
+            elem.download = filename;
+            document.body.appendChild(elem);
+            elem.click();
+            document.body.removeChild(elem);
+        }
     }
 
-    private static loadFromLocalStorage() {
-        return window.localStorage.getItem(this.assetsKey);
+    private static getLocalStorageKey() {
+        const key = window.sessionStorage.getItem(this.sessionStorageKey);
+
+        if (key) {
+            return key;
+        }
+
+        const keys = new Array<number>();
+
+        for (let index = 0; index < window.localStorage.length; ++index) {
+            const localStorageKey = window.localStorage.key(index);
+
+            if (localStorageKey) {
+                const localStorageKeyNumber = Number.parseInt(localStorageKey);
+
+                if (localStorageKeyNumber) {
+                    keys.push(localStorageKeyNumber);
+                }
+            }
+        }
+
+        if (keys.length === 0) {
+            return undefined;
+        }
+
+        return keys.reduce((m, c) => (c > m) ? c : m, Number.NEGATIVE_INFINITY).toString();
+    }
+
+    private static saveToLocalStorage(model: Model) {
+        if (model.groups.length === 0) {
+            // No point in storing empty models. Otherwise, local storage would always contain the same number of items
+            // as the maximum number of Asset Manager tabs the user ever had open.
+            return;
+        }
+
+        // Apparently, there's no reliable way to prevent the user from closing a browser window that contains unsaved
+        // changes. Both Chromium and Firefox currently refuse to show a confirmation dialog that is opened in a
+        // "beforeunload" handler and the documentation for the event warns that this could be the case. Therefore, the
+        // only way to prevent the loss of unsaved data is to save our data either continuously or in the "beforeunload"
+        // handler. We could either save through window.localStorage or download to the users hard drive (via the same
+        // mechanism as when the user invokes the "Save..." menu entry). However, the latter is bound to surprise at
+        // least some users and doesn't seem to work on some browsers anyway (e.g. Firefox). Our only remaining option
+        // is to therefore save to local storage.
+        // Obviously, just saving to local storage under one key (e.g. "assets") only works reliably if the user
+        // confines herself to not ever work with Asset Manager in more than one browser window. Since we seem to have
+        // no way of preventing or detecting use in multiple browser windows, the storage mechanism must ideally satisfy
+        // the following requirements:
+        // 1. No committed but as of yet unsaved change in any browser window is ever lost if the user closes a tab,
+        //    closes the whole browser or reloads the page at any time.
+        // 2. It must be trivial to regain access to unsaved changes.
+        // It appears that both requirements can be satisfied with the following mechanism:
+        // - Data is always saved to local storage in a "beforeunload" handler under a monotonously increasing key,
+        //   which is then also stored through window.sessionStorage.
+        // - When the Asset Manager page loads, session storage is first checked for a saved key. If one exists, data
+        //   under said key is then loaded from local storage. This ensures that the same data is always displayed
+        //   before and after a page reload. If session storage does not contain a key, data under the largest key is
+        //   loaded from local storage.
+        // - Successfully loaded data is always *deleted* from local storage immediately. This ensures that navigating
+        //   to Asset Manager in additional browser windows will never load the same data in more than one browser
+        //   window.
+        // The above should ensure that Asset Manager can be used much like a normal desktop application. Even if the
+        // application is used in multiple tabs when the browser is closed, data in said tabs will be reloaded next
+        // time the user navigates to the Asset Manager in the same number of tabs/windows. This is nicely complemented
+        // by the fact that many browsers have an option to automatically reload all previously open tabs when the
+        // browser is started.
+        const json = model.toJsonString();
+        let key: string | undefined;
+
+        // tslint:disable-next-line:no-empty
+        while (!(key = this.trySaveToLocalStorage(json))) {
+        }
+
+        window.sessionStorage.setItem(this.sessionStorageKey, key);
+    }
+
+    private static trySaveToLocalStorage(json: string) {
+        // We need a key that is unique and monotonously increasing. When a browser is closed with the application
+        // running in multiple tabs, it is likely that two tabs will get the same value from Date.now(). The random
+        // component should reduce the likelihood of a collision.
+        const uniqueNumber = Math.trunc((Date.now() + Math.random()) * Math.pow(2, 10));
+
+        if (!Number.isSafeInteger(uniqueNumber)) {
+            // Make sure all digits are significant. Since Date.now() currently returns a value with 41 binary digits,
+            // adding another 10 binary digits should keep us well below the 53 binary digits commonly supported for
+            // safe integers.
+            throw new Error("Can't create unique key.");
+        }
+
+        const uniqueKey = uniqueNumber.toString();
+
+        if (window.localStorage.getItem(uniqueKey)) {
+            return undefined;
+        }
+
+        window.localStorage.setItem(uniqueKey, json);
+
+        // There's still a low probability that two browser tabs arrive at the same key at the same time and an
+        // overwrite happens. Verifying the written should reduce this further.
+        return window.localStorage.getItem(uniqueKey) === json ? uniqueKey : undefined;
     }
 
     private get fileInput() {
@@ -152,7 +261,12 @@ export default class App extends Vue {
         return model;
     }
 
+    private onBeforeUnload(ev: BeforeUnloadEvent) {
+        App.saveToLocalStorage(this.model);
+    }
+
+    // tslint:disable-next-line:prefer-function-over-method
     private onModelChanged() {
-        App.saveToLocalStorage(this.model.toJsonString());
+        // App.saveToLocalStorage(this.model.toJsonString());
     }
 }
