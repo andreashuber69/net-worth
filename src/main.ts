deb8f727c82552c6f45ecb8b32b3a11707d10bbd
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-07-24 10:19:18 +0200

    Add incompatible browser scaffolding
    References #41

--- src/main.ts
+++ src/main.ts
@@ -17,30 +17,52 @@ import Vuetify from "vuetify";
 // tslint:disable-next-line:no-import-side-effect no-implicit-dependencies no-submodule-imports
 import "vuetify/dist/vuetify.min.css";
 import App from "./App.vue";
+import { Browser } from "./Browser";
 
-const currentUrl = new URL(window.location.href);
-let hasParams = false;
+if (!Browser.isCompatible) {
+    const appElement = document.getElementById("app");
 
-// tslint:disable-next-line:no-unsafe-any
-for (const key of (currentUrl.searchParams as any).keys()) {
-    // tslint:disable-next-line:no-unsafe-any
-    const value = currentUrl.searchParams.get(key);
-
-    if (value) {
-        // tslint:disable-next-line:no-unsafe-any
-        window.sessionStorage.setItem(key, value);
-        hasParams = true;
+    if (appElement) {
+        appElement.innerHTML = `
+            <div style="font-size:large">
+                <p><strong>Asset Manager doesn't work on this browser.</strong></p>
+                <p>
+                    It is recommended to use Asset Manager from within a recent version of an
+                    <strong>open-source</strong> browser like <strong>Chromium</strong> or <strong>Firefox</strong>.
+                </p>
+                <p>
+                    Asset Manager should also work on recent incarnations of proprietary browsers like Chrome, Opera,
+                    Safari, Edge and probably others.
+                </p>
+                <p>(${window.navigator.userAgent})</p>
+            </div>
+        `;
     }
-}
-
-if (hasParams) {
-    window.location.replace(window.location.href.split("?")[0]);
 } else {
-    // tslint:disable:no-unsafe-any
-    Vue.config.productionTip = false;
-    Vue.use(Vuetify);
+    const currentUrl = new URL(window.location.href);
+    let hasParams = false;
 
-    new Vue({
-      render: (h) => h(App),
-    }).$mount("#app");
+    // tslint:disable-next-line:no-unsafe-any
+    for (const key of (currentUrl.searchParams as any).keys()) {
+        // tslint:disable-next-line:no-unsafe-any
+        const value = currentUrl.searchParams.get(key);
+
+        if (value) {
+            // tslint:disable-next-line:no-unsafe-any
+            window.sessionStorage.setItem(key, value);
+            hasParams = true;
+        }
+    }
+
+    if (hasParams) {
+        window.location.replace(window.location.href.split("?")[0]);
+    } else {
+        // tslint:disable:no-unsafe-any
+        Vue.config.productionTip = false;
+        Vue.use(Vuetify);
+
+        new Vue({
+          render: (h) => h(App),
+        }).$mount("#app");
+    }
 }
