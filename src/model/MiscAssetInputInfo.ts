bcd635140d16d5b39c7c0f2720fa2afeaa914994
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-07-29 11:45:38 +0200

    Allow 0 as value for a miscellaneous asset
    References #32

--- src/model/MiscAssetInputInfo.ts
+++ src/model/MiscAssetInputInfo.ts
@@ -34,7 +34,7 @@ export class MiscAssetInputInfo extends AssetInputInfo {
     public readonly fineness = new TextInputInfo();
     public readonly value = new TextInputInfo(
         "Value", "The value of a single item, expressed in Currency.", true, true,
-        MiscAssetInputInfo.valueStep, undefined, MiscAssetInputInfo.valueStep);
+        0, undefined, MiscAssetInputInfo.valueStep);
     public readonly valueCurrency = new SelectInputInfo(
         "Currency", "The currency Value is expressed in.", true, true, Currency, true);
     public readonly quantity = new TextInputInfo("Quantity", "The number of items.", true, true, 0);
