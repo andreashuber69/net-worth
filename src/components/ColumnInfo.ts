92b6024561791bca69b394b2901254c57d64b245
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-08-19 18:32:28 +0200

    Mute some CB issues

--- src/components/ColumnInfo.ts
+++ src/components/ColumnInfo.ts
@@ -124,6 +124,7 @@ export class ColumnInfo {
 
     // Obviously the metrics could be improved by breaking the method into multiple parts but doing so would make the
     // code less readable.
+    // codebeat:disable[abc,cyclo,loc]
     private static getPadding(columnName: string | undefined, groupBy: string, otherGroupBys: GroupBy[]) {
         const columnPadding = 3;
         const leftClass = `pl-${columnPadding}`;
@@ -163,6 +164,7 @@ export class ColumnInfo {
                 return [];
         }
     }
+    // codebeat:enable[abc,cyclo,loc]
 
     private static addAlignment(result: string[], columnName: string | undefined) {
         switch (columnName) {
