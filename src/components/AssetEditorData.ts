f72cf4b27086a1ad1e07ed6cf5be4e8b0abada16
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-08-20 06:35:50 +0200

    Reduce scores for 2 CB issues

--- src/components/AssetEditorData.ts
+++ src/components/AssetEditorData.ts
@@ -14,6 +14,7 @@ import { IAssetUnion } from "../model/AssetInterfaces";
 import { CryptoWallet } from "../model/CryptoWallet";
 import { Currency } from "../model/Currency";
 import { IAuxProperties } from "../model/IAuxProperties";
+import { MiscAsset } from "../model/MiscAsset";
 import { PreciousMetalAsset } from "../model/PreciousMetalAsset";
 import { WeightUnit } from "../model/WeightUnit";
 
@@ -35,33 +36,13 @@ export class AssetEditorData implements IAuxProperties<string> {
         if (asset) {
             this.description = asset.description;
             this.location = asset.location || "";
-
-            if (asset.superType === PreciousMetalAsset.superType) {
-                this.address = "";
-                this.weight = asset.weight.toString();
-                this.weightUnit = WeightUnit[asset.weightUnit] as keyof typeof WeightUnit;
-                this.fineness = asset.fineness.toString();
-                this.value = "";
-                this.valueCurrency = "";
-                this.quantity = AssetEditorData.getQuantity(asset);
-            } else if (asset.superType === CryptoWallet.superType) {
-                this.address = asset.address || "";
-                this.weight = "";
-                this.weightUnit = "";
-                this.fineness = "";
-                this.value = "";
-                this.valueCurrency = "";
-                this.quantity = this.address ? "" : AssetEditorData.getQuantity(asset);
-            } else {
-                this.address = "";
-                this.weight = "";
-                this.weightUnit = "";
-                this.fineness = "";
-                this.value = asset.value.toString();
-                this.valueCurrency = asset.valueCurrency;
-                this.quantity = AssetEditorData.getQuantity(asset);
-            }
-
+            this.address = AssetEditorData.getAddress(asset);
+            this.weight = AssetEditorData.getWeight(asset);
+            this.weightUnit = AssetEditorData.getWeightUnit(asset);
+            this.fineness = AssetEditorData.getFineness(asset);
+            this.value = AssetEditorData.getValue(asset);
+            this.valueCurrency = AssetEditorData.getValueCurrency(asset);
+            this.quantity = AssetEditorData.getQuantity(asset);
             this.notes = asset.notes || "";
         } else {
             this.description = "";
@@ -79,7 +60,36 @@ export class AssetEditorData implements IAuxProperties<string> {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    private static getAddress(asset: IAssetUnion) {
+        return asset.superType === CryptoWallet.superType ? asset.address || "" : "";
+    }
+
+    private static getWeight(asset: IAssetUnion) {
+        return asset.superType === PreciousMetalAsset.superType ? asset.weight.toString() : "";
+    }
+
+    private static getWeightUnit(asset: IAssetUnion) {
+        return asset.superType === PreciousMetalAsset.superType ?
+            WeightUnit[asset.weightUnit] as keyof typeof WeightUnit : "";
+    }
+
+    private static getFineness(asset: IAssetUnion) {
+        return asset.superType === PreciousMetalAsset.superType ? asset.fineness.toString() : "";
+    }
+
+    private static getValue(asset: IAssetUnion) {
+        return asset.superType === MiscAsset.superType ? asset.value.toString() : "";
+    }
+
+    private static getValueCurrency(asset: IAssetUnion) {
+        return asset.superType === MiscAsset.superType ? asset.valueCurrency : "";
+    }
+
     private static getQuantity(asset: IAssetUnion) {
-        return asset.quantity !== undefined ? asset.quantity.toString() : "";
+        if ((asset.superType === CryptoWallet.superType) && asset.address) {
+            return "";
+        } else {
+            return asset.quantity !== undefined ? asset.quantity.toString() : "";
+        }
     }
 }
