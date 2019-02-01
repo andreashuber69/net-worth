8c44d1366985d196a1a4bf007e7dc0230520cea6
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-02-01 21:34:28 +0100

    Add allAssets prefetch (commented out)

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@
 // <http://www.gnu.org/licenses/>.
 
 const OfflinePlugin = require("offline-plugin");
+// const PreloadPlugin = require("@vue/preload-webpack-plugin");
 
 module.exports = {
     // publicPath needs to be set differently depending on whether we build for actual deployment (e.g. on github
@@ -28,5 +29,13 @@ module.exports = {
     },
     chainWebpack: config => {
         config.plugin("offline-plugin").use(OfflinePlugin);
+
+        // The following instructs webpack to prefetch all assets (including fonts), which could theoretically speed up
+        // the very first page load. TODO: Test whether this is the case.
+        // // cSpell: ignore prefetch
+        // config.plugin("prefetch").use(PreloadPlugin, [{
+        //     rel: "prefetch",
+        //     include: "allAssets"
+        // }]);
     }
 }
