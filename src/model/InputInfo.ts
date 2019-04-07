29194a2eab688e4afdc290a566a4f592f464c0c8
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-04-07 13:53:56 +0200

    Fix docs

--- src/model/InputInfo.ts
+++ src/model/InputInfo.ts
@@ -22,8 +22,8 @@ import { Unknown } from "./Unknown";
  */
 export abstract class InputInfo {
     /**
-     * Validates `value` or a property of `value`.
-     * @description As the type indicates, `value` can be of primitive or composite type. In the former case, `value`
+     * Validates `input` or a property of `input`.
+     * @description As the type indicates, `input` can be of primitive or composite type. In the former case, `input`
      * itself is validated. In the latter case, the value of the property with the passed name is validated. Either
      * way, this method only ever validates a single primitive value.
      * @returns `true` if the property value is valid; otherwise a string describing why the value is invalid.
