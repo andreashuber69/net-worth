ee1a675806c7c7d6af77eb18e390d28707ca6ad2
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-04-15 14:30:03 +0200

    Add line break

--- src/model/Erc20TokensWalletBundle.ts
+++ src/model/Erc20TokensWalletBundle.ts
@@ -86,7 +86,9 @@ export class Erc20TokensWalletBundle extends AssetBundle {
     // tslint:disable-next-line: max-classes-per-file variable-name
     private static readonly GetAddressInfoQuery = class NestedQuery extends Query<EthplorerGetAddressInfoResponse> {
         public constructor(address: string) {
-            super(`https://api.ethplorer.io/getAddressInfo/${address}?apiKey=dvoio1769GSrYx63`, EthplorerGetAddressInfoResponse);
+            super(
+                `https://api.ethplorer.io/getAddressInfo/${address}?apiKey=dvoio1769GSrYx63`,
+                EthplorerGetAddressInfoResponse);
         }
     };
 
