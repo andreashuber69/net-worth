45fd6945be18061a7e53a99a5cf523eba5d5ca31
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-07-16 09:13:00 +0200

    refactor(validation): remove dependency to AssetPropertyNames

--- src/model/validation/schemas/GroupBy.ts
+++ src/model/validation/schemas/GroupBy.ts
@@ -10,6 +10,6 @@
 // You should have received a copy of the GNU General Public License along with this program. If not, see
 // <http://www.gnu.org/licenses/>.
 
-import { AssetPropertyNames } from "../../AssetPropertyNames";
+import { IAsset } from "./IAssetProperties";
 
-export type GroupBy = typeof AssetPropertyNames.type | typeof AssetPropertyNames.location;
+export type GroupBy = keyof Pick<IAsset, "type" | "location">;
