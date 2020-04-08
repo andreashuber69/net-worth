c1dac8cf92bdf39f500b65382766e29fe88a44e5
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2020-04-08 08:08:51 +0200

    test(FastXpub): extract fixture

--- src/model/FastXpub.spec.ts
+++ src/model/FastXpub.spec.ts
@@ -4,30 +4,39 @@ import { HDNode, networks } from "@trezor/utxo-lib";
 import { FastXpub } from "./FastXpub";
 
 // cSpell: disable
+const deriveAddressRangeFixtures = [
+    {
+        coin: "legacy BTC",
+        network: networks.bitcoin,
+        xpub: "xpub6CR3KoDQXWzSRJJvUq9Kr2rxGCHAfuwTeQAP3Q4p3687zxwiukfjRYZhgNxugYBjxk9iCAqy1mWR8Hb7oqqYUneYoZajA4CnzQyWhv52pHq",
+        expectedAddresses: [
+            "19WxeWSpHH3YKNqPia7BpfGgea4GKT9ENJ",
+            "1FoJtisysDzJ92QMDSCtHoPSV7sN5sjeh7",
+            "16xCaBhJSyQaniRnN3NqiW1ohgaxdoansU",
+        ] as const,
+    },
+] as const;
+
 describe(FastXpub.name, () => {
+    // eslint-disable-next-line init-declarations
+    let sut: FastXpub;
+    beforeAll(async () => (sut = await FastXpub.create()));
+
     describe("deriveNode", () => {
         it("should derive the correct nodes", async () => {
-            const sut = await FastXpub.create();
-            const node = HDNode.fromBase58("xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB", networks.bitcoin);
+            const node = HDNode.fromBase58("xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB");
             const m0 = await sut.deriveNode(node, 0);
             expect(m0).toEqual("xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH");
         });
     });
-    describe("deriveAddressRange", () => {
-        it("should derive the correct addresses", async () => {
-            const sut = await FastXpub.create();
-            const node = HDNode.fromBase58("xpub6EikzRzgQiSsjiL3nz2SUZTi964wvqa1Je1WtRr5cXEwFEbMJvhas6BauDXoAVRhShfrJVTF1CKoq8zFmA9x72KocKxmRPBy4DizNPxLw9g", networks.bitcoin);
-            const addresses = await sut.deriveAddressRange(node, 0, undefined, 0, 2);
-            expect(addresses).toEqual(["1LDB6EomUoB4uXaqutFes9uduQ9Mz7PgHh", "1CfZgDBKAy9W6o4wJiGjCeMe4HPb3sibsY", "1L5CaYTqgyimJwT8RY2WBBtMuVxzPdH2X3"]);
-        });
-    });
-    describe("deriveAddressRange", () => {
-        it("should derive the correct addresses for a legacy BTC wallet", async () => {
-            const sut = await FastXpub.create();
-            const parent = HDNode.fromBase58("xpub6CR3KoDQXWzSRJJvUq9Kr2rxGCHAfuwTeQAP3Q4p3687zxwiukfjRYZhgNxugYBjxk9iCAqy1mWR8Hb7oqqYUneYoZajA4CnzQyWhv52pHq", networks.bitcoin);
-            const m0 = HDNode.fromBase58(await sut.deriveNode(parent, 0));
-            const addresses = await sut.deriveAddressRange(m0, 0, undefined, 0, 2);
-            expect(addresses).toEqual(["19WxeWSpHH3YKNqPia7BpfGgea4GKT9ENJ", "1FoJtisysDzJ92QMDSCtHoPSV7sN5sjeh7", "16xCaBhJSyQaniRnN3NqiW1ohgaxdoansU"]);
+
+    deriveAddressRangeFixtures.forEach((f) => {
+        describe("deriveAddressRange", () => {
+            it(`should derive the correct addresses for a ${f.coin} wallet`, async () => {
+                const parent = HDNode.fromBase58(f.xpub, f.network);
+                const m0 = HDNode.fromBase58(await sut.deriveNode(parent, 0));
+                expect(await sut.deriveAddressRange(m0, 0, undefined, 0, 2)).toEqual(f.expectedAddresses);
+            });
         });
     });
 });
