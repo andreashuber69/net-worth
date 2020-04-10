cffe14716003cbc5fabb6d9ef63f672462723d78
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2020-04-10 23:34:40 +0200

    refactor(BtcWallet): extract class

--- src/model/BtcWallet.ts
+++ src/model/BtcWallet.ts
@@ -1,9 +1,9 @@
 // https://github.com/andreashuber69/net-worth#--
 /* eslint-disable max-classes-per-file */
 import { networks } from "@trezor/utxo-lib";
-import { FastXpub } from "./FastXpub";
 import { IParent } from "./IEditable";
 import { IWebRequest } from "./IWebRequest";
+import { IBatchInfo, QuantityRequest } from "./QuantityRequest";
 import { QueryCache } from "./QueryCache";
 import { QueryError } from "./QueryError";
 import { RealCryptoWallet } from "./RealCryptoWallet";
@@ -11,12 +11,6 @@ import { SimpleCryptoWallet } from "./SimpleCryptoWallet";
 import { bitcoin } from "./validation/schemas/AssetTypeName.schema";
 import { BlockchainBalanceResponse, IAddressBalance } from "./validation/schemas/BlockchainBalanceResponse.schema";
 import { ISimpleCryptoWalletProperties } from "./validation/schemas/ISimpleCryptoWalletProperties.schema";
-
-/** @internal */
-interface IBatchInfo {
-    balance: number;
-    txCount: number;
-}
 
 /** Represents a BTC wallet. */
 export class BtcWallet extends SimpleCryptoWallet {
@@ -31,7 +25,7 @@ export class BtcWallet extends SimpleCryptoWallet {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     protected async queryQuantity() {
-        return new BtcWallet.QuantityRequest(this.address).queryQuantity();
+        return new BtcWallet.BtcQuantityRequest(this.address).queryQuantity();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,52 +71,16 @@ export class BtcWallet extends SimpleCryptoWallet {
         private readonly addresses: string;
     };
 
-    private static readonly QuantityRequest = class NestedQuantityRequest {
-        public constructor(private readonly address: string) {
-        }
-
-        public async queryQuantity() {
-            // TODO: This is a crude test to distinguish between xpub and a normal address
-            if (this.address.length <= 100) {
-                await this.getBatchInfo([this.address]);
-            } else {
-                await Promise.all([
-                    this.addChain(await this.fastXpub.deriveNode(this.address, 0)),
-                    this.addChain(await this.fastXpub.deriveNode(this.address, 1)),
-                ]);
-            }
-
-            return this.quantity;
+    private static readonly BtcQuantityRequest = class extends QuantityRequest {
+        public constructor(address: string) {
+            super(networks.bitcoin, address);
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        private static readonly batchLength = 20;
-        private readonly fastXpub = new FastXpub(networks.bitcoin);
-        private quantity = 0;
-
         // eslint-disable-next-line class-methods-use-this
-        private async getBatchInfo(addresses: readonly string[]) {
+        protected async getBatchInfo(addresses: readonly string[]) {
             return new BtcWallet.BlockchainRequest(addresses).execute();
-        }
-
-        private async addChain(xpub: string) {
-            let done = false;
-            let batch = await this.getBatch(xpub, 0);
-
-            for (let index = NestedQuantityRequest.batchLength; !done; index += NestedQuantityRequest.batchLength) {
-                // We need to do this sequentially such that we don't miss the point where unused addresses start
-                // eslint-disable-next-line no-await-in-loop
-                const results = await Promise.all([this.getBatchInfo(batch), this.getBatch(xpub, index)]);
-                const [{ balance, txCount }] = results;
-                this.quantity += balance;
-                done = txCount === 0;
-                [, batch] = results;
-            }
-        }
-
-        private async getBatch(xpub: string, index: number) {
-            return this.fastXpub.deriveAddressRange(xpub, index, index + NestedQuantityRequest.batchLength - 1);
         }
     };
 }
---
+++ src/model/QuantityRequest.ts
@@ -0,0 +1,58 @@
+// https://github.com/andreashuber69/net-worth#--
+import { Network } from "@trezor/utxo-lib";
+import { FastXpub } from "./FastXpub";
+
+export interface IBatchInfo {
+    balance: number;
+    txCount: number;
+}
+
+export abstract class QuantityRequest {
+    public async queryQuantity() {
+        // TODO: This is a crude test to distinguish between xpub and a normal address
+        if (this.address.length <= 100) {
+            await this.getBatchInfo([this.address]);
+        } else {
+            await Promise.all([
+                this.addChain(await this.fastXpub.deriveNode(this.address, 0)),
+                this.addChain(await this.fastXpub.deriveNode(this.address, 1)),
+            ]);
+        }
+
+        return this.quantity;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    protected constructor(network: Network, private readonly address: string) {
+        this.fastXpub = new FastXpub(network);
+    }
+
+    protected abstract getBatchInfo(addresses: readonly string[]): Promise<IBatchInfo>;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static readonly batchLength = 20;
+    private readonly fastXpub: FastXpub;
+    private quantity = 0;
+
+    private async addChain(xpub: string) {
+        let done = false;
+        let batch = await this.getBatch(xpub, 0);
+
+        for (let index = QuantityRequest.batchLength; !done; index += QuantityRequest.batchLength) {
+            // We need to do this sequentially such that we don't miss the point where unused addresses start
+            // eslint-disable-next-line no-await-in-loop
+            const results = await Promise.all([this.getBatchInfo(batch), this.getBatch(xpub, index)]);
+            const [{ balance, txCount }] = results;
+            this.quantity += balance;
+            done = txCount === 0;
+            [, batch] = results;
+        }
+    }
+
+    private async getBatch(xpub: string, index: number) {
+        return this.fastXpub.deriveAddressRange(xpub, index, index + QuantityRequest.batchLength - 1);
+    }
+}
+
